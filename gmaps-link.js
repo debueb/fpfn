ad1c7c3b5d5e06ebb122d87fe5d29c38f507fcca
debueb <debueb@users.noreply.github.com>
2022-07-27 08:16:39 +0200

    Update gmaps-link.js

--- gmaps-link.js
+++ gmaps-link.js
@@ -2,7 +2,7 @@ let geo = document.querySelector("[itemprop='geo']");
 let lat = document.querySelector("[itemprop='latitude']");
 let lon = document.querySelector("[itemprop='longitude']");
 let btn = document.createElement('button');
-btn.onclick = () => { open(` http://maps.google.de/maps?q=${lat.textContent},${lon.textContent}&t=k&z=19`) };
+btn.onclick = () => { open(` https://www.google.de/maps?q=${lat.textContent},${lon.textContent}&t=k&z=19`) };
 btn.textContent = 'Open in Google Maps';
 btn.className = 'ip4n';
 geo.appendChild(btn);
