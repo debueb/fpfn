93a3d47bae2d736c32f05c9ea5a17b468453ec58
debueb <d.wisskirchen@gmail.com>
2023-05-21 15:30:54 +0200

    fix gmaps link

--- gmaps-link.js
+++ gmaps-link.js
@@ -1,8 +1,11 @@
-let geo = document.querySelector("[itemprop='geo']");
-let lat = document.querySelector("[itemprop='latitude']");
-let lon = document.querySelector("[itemprop='longitude']");
+let ul = document.getElementsByClassName('place-info-location mt-5 mb-4').item(0);
+let latLon = ul.children.item(0).children.item(1).children.item(0).textContent; //42.5108, 0.1249 (lat, lng)'
+let lat = latLon.substring(0, latLon.indexOf(','));
+let lon = latLon.substring(latLon.indexOf(',')+1, latLon.indexOf('(')).trim();
+console.log(lat);
+console.log(lon);
 let btn = document.createElement('button');
-btn.onclick = () => { open(` https://www.google.de/maps?q=${lat.textContent},${lon.textContent}&t=k&z=19`) };
+btn.onclick = () => { open(` https://www.google.de/maps?q=${lat},${lon}&t=k&z=19`) };
 btn.textContent = 'Open in Google Maps';
 btn.className = 'ip4n';
-geo.appendChild(btn);
+ul.insertBefore(btn, ul.children.item(1));
